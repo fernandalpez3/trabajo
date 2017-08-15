3a6032b436ac69eed614c3af17922a53f2acf881
katiachumakova <katiachumakova95@gmail.com>
2017-08-15 18:33:53 -0500

    switch con parallax

--- SWITCH/wp-content/themes/somos-switch/functions.php
+++ SWITCH/wp-content/themes/somos-switch/functions.php
@@ -1,1 +1,5 @@
 <?php
+function somos_switch(){
+	wp_enqueue_script('extra js', get_stylesheet_directory_uri() . '/extra-js/jquery.jInvertScroll.min.js');
+}
+add_action('wp_enqueue_scripts', 'somos_switch');
